1ea0bce73f0d6d320ca1f60c5d9747af7370205f
Anshu3721 <anshu.kumar.ext@nokia.com>
2025-07-07 13:21:16 +0530

    second commit

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,12 @@
 # Routes package initialization 
+# app/__init__.py
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    return app
